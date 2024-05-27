8cb1c7ea48a3c9a3f477b1a6d7208f37e7df4a49
Erik Lopez <erik.lopez@qualuscorp.com>
2024-05-27 14:22:06 -0400

    adding files

---
+++ previewer.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+BATCMD="batcat"
+
+case "$1" in
+    *.tar*) tar tf "$1";;
+    *.zip) unzip -l "$1";;
+    *.rar) unrar l "$1";;
+    *.7z) 7z l "$1";;
+
+    *.pdf) pdftotext "$1" -;;
+	*.pptx) unzip -qc "$1" | grep -oP '(?<=\<a:t\>).*?(?=\</a:t\>)';;
+
+	*.png|*.jpg|*.jpeg|*.mkv|*.mp4|*.3gp) mediainfo "$1";;
+
+    *.csv) rich -h 50 --force-terminal --no-wrap --theme dracula "$1" ;;
+
+    *.ipynb) rich -h 50 --force-terminal --no-wrap --theme dracula "$1" ;;
+
+    *) $BATCMD -n --color always "$1" ;;
+esac
